743955a0e41c9340e01a143ce4a559f958b7d2f6
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-06-23 21:11:41 -0400

    Update Principal.js

--- Principal.js
+++ Principal.js
@@ -3,6 +3,10 @@ import { Button, View, Text,Image, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NuevoUsuario from './NuevoUsuario';
+import ListarUsuario from './ListarUsuarios';
+import ActualizarUsuario from './ActualizarUsuario';
+import { Ionicons } from "@expo/vector-icons";
+import { Title } from 'react-native-paper';
 
 export default  function Principal({navigation}){
   const Stack =createNativeStackNavigator();
@@ -17,9 +21,22 @@ export default  function Principal({navigation}){
     >
       <Stack.Screen
         name='Listar Usuarios'
-        component={NuevoUsuario}
+        component={ListarUsuario}
+        options={({ navigation, route }) => ({
+          title: 'Crud',
+          headerRight: () => (
+            <Ionicons
+              name={'ios-add-circle'}
+              size={25}
+              color={'white'}
+              style={{ marginRight: 15 }}
+              onPress={() => navigation.navigate('Nuevo Usuario')}
+            />
+          ),
+        })}
       />
-
+      <Stack.Screen name="Nuevo Usuario" component={NuevoUsuario} />
+      <Stack.Screen name="ActualizarUsuario" component={ActualizarUsuario} />
 
     </Stack.Navigator>
     
