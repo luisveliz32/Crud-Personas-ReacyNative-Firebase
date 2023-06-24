7a47864525637b9ec1f1431c5ac31e4b6ef9fe31
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-06-23 21:10:20 -0400

    Update NuevoUsuario.js

--- NuevoUsuario.js
+++ NuevoUsuario.js
@@ -1,9 +1,38 @@
 import  React ,{useState,useEffect} from 'react';
 import { View, TextInput,StyleSheet,Button ,Alert} from 'react-native';
 import Constants from 'expo-constants'; 
+import appFirebase from '../credenciales'
+import {getFirestore,collection,addDoc,getDocs,doc,deleteDoc,getDoc,setDoc} from 'firebase/firestore';
+
+const db=getFirestore(appFirebase);
 
 export default  function NuevoUsuario({navigation}){
-  
+  const [persona,setpersona]=useState(
+    {
+      nombre:'',
+      apellidop:'',
+      apellidom:'',
+      ci:'',
+      edad:'',
+      sexo:''
+    }
+  );
+  const caturandoInput=(value,name)=>{
+    setpersona({...persona,[name]:value})
+  }
+
+  const guardarpersona=async()=>{
+    //console.log(persona)
+    try {
+      await addDoc(collection(db,'personas'),{
+        ...persona
+      })
+      console.log('exito');
+    } catch (error) {
+      console.log(error)
+    }
+    navigation.navigate('Listar Usuarios')
+  }
   
   return(
     
@@ -11,26 +40,35 @@ export default  function NuevoUsuario({navigation}){
         <TextInput
           placeholder='Nombre'
           style={styles.input}
-          
+          onChangeText={(value)=>caturandoInput(value,'nombre')}
+          value={persona.nombre}
         />
         <TextInput
           placeholder='Apellido Paterno'
           style={styles.input}
-          
+          onChangeText={(value)=>caturandoInput(value,'apellidop')}
+          value={persona.apellidop}
+         
         />
         <TextInput
           placeholder='Apellido Materno'
           style={styles.input}
-          
+          onChangeText={(value)=>caturandoInput(value,'apellidom')}
+          value={persona.apellidom}
+         
         />
         <TextInput
-          placeholder='Documento de Identidad'
+          placeholder='Carnet de Identidad'
           style={styles.input}
-          
+          onChangeText={(value)=>caturandoInput(value,'ci')}
+          value={persona.ci}
+         
         />
+        
         <Button
           title='Guardar Usuario'
           style={styles.boton}
+          onPress={guardarpersona}
         />
     </View>
   );
