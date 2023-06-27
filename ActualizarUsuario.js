63e94031273d9bae12d67415cd12e7288dcb29ab
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-06-26 21:32:29 -0400

    Update ActualizarUsuario.js funcion eliminar agregado

--- ActualizarUsuario.js
+++ ActualizarUsuario.js
@@ -1,35 +1,66 @@
 import  React ,{useState,useEffect} from 'react';
 import { View, TextInput,StyleSheet,Button } from 'react-native';
 import Constants from 'expo-constants'; 
+import appFirebase from '../credenciales';
+import {getFirestore,collection,addDoc,getDocs,doc,deleteDoc,getDoc,setDoc} from 'firebase/firestore';
+const db = getFirestore(appFirebase);
 
-export default  function ActualizarUsuario({navigation}){
-  
+export default  function ActualizarUsuario({navigation,route}){
+  const { item } = route.params;
+  const[persona,setPersona]=useState({
+    id:item.id,
+    nombre:item.nombre,
+    apellidop:item.apellidop,
+    apellidom:item.apellidom,
+    ci:item.ci,
+  })
+  const actualizar=()=>{
+    console.log(persona)
+  }
+  const eliminar=async(id)=>{
+    try {
+      await deleteDoc(doc(db,'personas',id));
+    console.log('persona eliminada con exito');
+    navigation.navigate('Listar Usuarios')
+      
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
   return(
     
     <View style={styles.container} >
         <TextInput
           placeholder='Nombre'
           style={styles.input}
+          value={persona.nombre}
         />
         <TextInput
           placeholder='Apellido Paterno'
           style={styles.input}
+          value={persona.apellidop}
         />
         <TextInput
           placeholder='Apellido Materno'
           style={styles.input}
+          value={persona.apellidom}
         />
         <TextInput
           placeholder='Documento de Identidad'
           style={styles.input}
+          value={persona.ci}
         />
         <Button
+        color="blue"
         title='Actualizar Usuario'
             style={styles.boton}
         />
         <Button
         title='Elimiar Usuario'
-            style={styles.botoneliminar}
+        color="red"
+        onPress={()=>eliminar(persona.id)}
+            style={styles.boton}
         />
     </View>
   );
@@ -49,13 +80,9 @@ const styles=StyleSheet.create({
         margin:15 
     },
     boton:{
-        backgroundColor:'blue',
+      
         padding:10
     },
-    botoneliminar:{
-        backgroundColor:'red',
-        padding:10
-    }
 });
 
 
