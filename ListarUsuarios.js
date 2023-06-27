772b3bab2d2df0f58b4a06e24e4e5151f3dfa2df
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-06-26 21:34:33 -0400

    Update ListarUsuarios.js agregado listar usuario agregado

--- ListarUsuarios.js
+++ ListarUsuarios.js
@@ -1,14 +1,64 @@
 import  React ,{useState,useEffect} from 'react';
-import { View, TextInput,StyleSheet,Button ,Alert,Text} from 'react-native';
+import { View, TextInput,StyleSheet,Button ,Alert,Text,FlatList, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants'; 
+import appFirebase from '../credenciales'
+import {getFirestore,collection,addDoc,getDocs,doc,deleteDoc,getDoc,setDoc} from 'firebase/firestore';
+
+const db=getFirestore(appFirebase);
 
 export default  function ListarUsuarios({navigation}){
+  const [listar,setListar]=useState([]);
   
-  
+  const optenerlista=async()=>{
+    try {
+      const lista=await getDocs(collection(db,'personas'));
+      const docs=[];
+      lista.forEach((doc)=>{
+          const {nombre,apellidop,apellidom,ci}=doc.data()
+          docs.push(
+
+            {
+              id:doc.id,
+              nombre,
+              apellidop,
+              apellidom,
+              ci
+            }
+          )
+      })
+      setListar(docs);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    optenerlista();
+  },[listar])
+  const renderItem=({item})=>{
+    return (
+      <TouchableOpacity 
+        onPress={()=>navigation.navigate('Actualizar Usuario',{item})}
+      >
+          <View style={{borderBottomWidth:1,
+                borderBottomColor:'#ccc',
+                padding:5
+                }}>
+                  <Text style={{fontWeight:'bold'}}>Persona: {item.nombre} {item.apellidop} {item.apellidom}</Text>\
+                  <Text>Carnet de Identidad: {item.ci}</Text>
+                  <Text>Carnet de Identidad: {item.id}</Text>
+
+          </View>
+      </TouchableOpacity>
+    )
+  }
   return(
     
     <View style={styles.container} >
-            <Text>Listart Usuarios</Text>
+            <FlatList
+            data={listar}
+            renderItem={renderItem}
+            />
     </View>
   );
 }
@@ -31,7 +81,6 @@ const styles=StyleSheet.create({
         padding:10
     }
 });
-
 
 
 
